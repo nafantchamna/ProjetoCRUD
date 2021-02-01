7cf46114ae12acebe1a872986f002945734625bd
Cletche Nafantchamna <cletche1991@gmail.com>
2021-02-01 15:50:52 -0300

    tela de cadastro

--- Site/Site/App_Start/RouteConfig.cs
+++ Site/Site/App_Start/RouteConfig.cs
@@ -31,7 +31,7 @@ namespace Site
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index"}
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
